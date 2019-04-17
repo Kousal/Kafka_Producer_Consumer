a38eace504f360e74ef4750a8ef3b9ecd84a5010
kousel <mathi.kousel@gmail.com>
2019-04-16 21:50:25 -0400

    Prod consumer code

---
+++ kakfa-producer-consumer-example/src/main/java/ca/kousel/kafka/App.java
@@ -0,0 +1,82 @@
+package ca.kousel.kafka;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.concurrent.ExecutionException;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.TopicPartition;
+
+import ca.kousel.kafka.constants.IKafkaConstants;
+import ca.kousel.kafka.consumer.ConsumerCreator;
+import ca.kousel.kafka.producer.ProducerCreator;
+
+public class App {
+	public static void main(String[] args) {
+		//runProducer();
+		runConsumer();
+	}
+
+	static void runConsumer() {
+		Consumer<Long, String> consumer = ConsumerCreator.createConsumer();
+		consumer.subscribe(Collections.singletonList(IKafkaConstants.TOPIC_NAME));
+		// User either below code or subscribe method call 
+		/*TopicPartition tp = new TopicPartition("3partition_2replication", 0);
+		ArrayList al = new ArrayList();
+		al.add(tp);
+		consumer.assign(al);*/
+		int noMessageToFetch = 0;
+
+		while (true) {
+			final ConsumerRecords<Long, String> consumerRecords = consumer.poll(1000);
+			if (consumerRecords.count() == 0) {
+				noMessageToFetch++;
+				if (noMessageToFetch > IKafkaConstants.MAX_NO_MESSAGE_FOUND_COUNT)
+					break;
+				else
+					continue;
+			}
+		
+			consumerRecords.forEach((record) -> {
+				System.out.println("Record Key " + record.key());
+				System.out.println("Record value " + record.value());
+				System.out.println("Record partition " + record.partition());
+				System.out.println("Record offset " + record.offset());
+			});
+			consumer.commitAsync();
+		}
+		consumer.close();
+	}
+
+	static void runProducer() {
+		Producer<Long, String> producer = ProducerCreator.createProducer();
+
+		for (int index = 0; index < IKafkaConstants.MESSAGE_COUNT; index++) {
+			
+			//Record without key
+			final ProducerRecord<Long, String> record = new ProducerRecord<Long, String>
+			(IKafkaConstants.TOPIC_NAME,
+					"This is record " + index);
+			
+			//Record with key being passed
+			final ProducerRecord<Long,String> rec1 = new ProducerRecord<Long,String>(IKafkaConstants.TOPIC_NAME,
+					new Long(index), "With key "+index);
+			try {
+				//RecordMetadata metadata = producer.send(record).get();
+				RecordMetadata metadata = producer.send(rec1).get();
+				System.out.println("Record sent with key " + index + " to partition " + metadata.partition()
+						+ " with offset " + metadata.offset());
+			} catch (ExecutionException e) {
+				System.out.println("Error in sending record");
+				System.out.println(e);
+			} catch (InterruptedException e) {
+				System.out.println("Error in sending record");
+				System.out.println(e);
+			}
+		}
+	}
+}
---
+++ kakfa-producer-consumer-example/src/main/java/ca/kousel/kafka/constants/IKafkaConstants.java
@@ -0,0 +1,21 @@
+package ca.kousel.kafka.constants;
+
+public interface IKafkaConstants {
+	public static String KAFKA_BROKERS = "localhost:9091,localhost:9092";
+	
+	public static Integer MESSAGE_COUNT=10;
+	
+	public static String CLIENT_ID="client1";
+	
+	public static String TOPIC_NAME="3partition_2replication";
+	
+	public static String GROUP_ID_CONFIG="consumerGroup10";
+	
+	public static Integer MAX_NO_MESSAGE_FOUND_COUNT=100;
+	
+	public static String OFFSET_RESET_LATEST="latest";
+	
+	public static String OFFSET_RESET_EARLIER="earliest";
+	
+	public static Integer MAX_POLL_RECORDS=1;
+}
